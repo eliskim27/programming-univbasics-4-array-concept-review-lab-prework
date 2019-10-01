079632a243ab990303ceb5dc4f1ef1b93a2a9e02
Eli Kim <eliskim27@gmail.com>
2019-10-01 00:57:31 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,6 +1,7 @@
 def find_element_index(array, value_to_find)
-  array.length.times do |var|
-    if array[]
+  array.length.times do |index|
+    if array[index] == value_to_find
+      
 end
 
 def find_max_value(array)
